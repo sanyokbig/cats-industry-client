3d4cfa3b3c4422cda8d69a53dc32b1299c716b36
sanyokbig <sanyokbig@gmail.com>
2018-02-05 18:50:04 +0300

    renamed fiedl

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -9,7 +9,7 @@ const login = () => {
     const settings = "menubar=0,width=" + width + ",height=" + height + ",left=" + x + ",top=" + y;
     window.open("", "eveAuth", settings);
 
-    ws.Send("login_request", {scopes: "simple"});
+    ws.Send("login_request", {scope_set: "simple"});
 };
 
 export {
