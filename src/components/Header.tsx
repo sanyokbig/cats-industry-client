e9d2a5e69150f96225de5e1a0b2f0bc46967e552
sanyokbig <sanyokbig@gmail.com>
2018-02-08 15:41:56 +0300

    portrait displayed instead of login button if loggedIn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
-import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Button} from "reactstrap";
+import {Button, Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from "reactstrap";
 import {Link} from "react-router-dom";
 import {login} from "../auth/login";
 import WsState from "./WsState";
+import {AppState} from "../state/store";
+import {connect} from "react-redux";
+import {User} from "../schema/user";
+import {Portrait} from "./Portrait";
 
-export default class Header extends React.Component {
+interface HeaderProps {
+    loggedIn?: boolean;
+    user?: User | null;
+}
+
+class Header extends React.Component {
+    props: HeaderProps;
+
     state: {
         isOpen: boolean
     };
 
-    constructor(props: object) {
+    constructor(props: HeaderProps) {
         super(props);
 
         this.state = {
@@ -25,6 +36,14 @@ export default class Header extends React.Component {
     };
 
     render() {
+        const {loggedIn, user} = this.props;
+        var mainID: number = 0;
+        if (user) {
+            var main = user.characters.filter(c => c.is_main);
+            if (main.length) {
+                mainID = main[0].id;
+            }
+        }
         return (
             <Navbar color="primary" dark expand="md">
                 <NavbarBrand tag={Link} to="/">Cats-industry</NavbarBrand>
@@ -35,7 +54,12 @@ export default class Header extends React.Component {
                             <NavLink tag={Link} to="/user">User</NavLink>
                         </NavItem>
                         <NavItem>
-                            <Button onClick={login}>Login</Button>
+                            {
+                                loggedIn
+                                    ? <Portrait characterID={mainID} size={"32"}/>
+                                    : <Button onClick={login}>Login</Button>
+
+                            }
                         </NavItem>
                     </Nav>
                 </Collapse>
@@ -44,3 +68,11 @@ export default class Header extends React.Component {
         );
     }
 }
+
+const mapStateToProps = (state: AppState) => {
+    const {loggedIn, user} = state.auth;
+
+    return {loggedIn, user};
+};
+
+export default connect<{}, {}, HeaderProps>(mapStateToProps)(Header);
